5a9c73a1d27de1604146c61511704de25fad818a
alfredo villasenor <alfredo.villasenor.aof@gmail.com>
2020-04-17 13:06:35 -0700

    finished

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,42 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
+import {connect} from 'react-redux';
 import "./App.css";
-class App extends Component {
-  render() {
+import "./App.css";
+import {fetchSmurf} from '../actions/Actions';
+import Smurform from './SmurfInput';
+import Smurfville from './Smurfville';
+
+
+
+
+const App= ({fetchSmurf})=> {
+
+  useEffect(() => {
+      fetchSmurf();
+    }, [fetchSmurf]);
+
+  
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+          <Smurfville />
+          <Smurform />
       </div>
     );
-  }
-}
+  };
+  const mapStateToProps = state => {
+    return {
+      message: state.message,
+      smurfs: state.smurfs
+    };
+  };
+  
+  export default connect(
+    mapStateToProps,
+    { fetchSmurf }
+  )(App);
 
-export default App;
+// export default App;
